a79df50024c5b3856df6095affbb1180e16b0e9a
Maxime Sebbah <maxime@darwinlabs.ai>
2024-11-28 22:08:34 +0100

    feat: use our stable fork of DeepSpeed
    - Replace Microsoft DeepSpeed with our stable fork
    - Remove specific commit checkout
    - Keep MI300 specific configurations

--- scripts/install_deepspeed.sh
+++ scripts/install_deepspeed.sh
@@ -8,13 +8,13 @@ pip uninstall deepspeed -y
 
 # Set maximum number of jobs for compilation
 export MAX_JOBS="$(nproc)"
+
 # Set architecture for PyTorch
 export PYTORCH_ROCM_ARCH="gfx90a;gfx942"
 
-# Clone to a specific commit in a temporary folder
-git clone https://github.com/microsoft/DeepSpeed.git tmp_deepspeed
+# Clone our stable fork
+git clone https://github.com/AI-DarwinLabs/deepspeed-stable-mi300.git tmp_deepspeed
 cd tmp_deepspeed
-git checkout $DEEPSPEED_COMMIT
 
 # Install with the same options as the original script
 pip install --verbose --no-cache-dir --no-build-isolation ./
