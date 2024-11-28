268988f471626b1691e22fdba025e11bda0b35a2
Maxime Sebbah <maxime@darwinlabs.ai>
2024-11-28 21:58:31 +0100

    refactor: improve installation scripts
    - Add Axolotl archive extraction step
    - Remove wget for bitsandbytes and use vendor file
    - Translate DeepSpeed comments to English and add PyTorch architecture comment

--- scripts/install_axolotl.sh
+++ scripts/install_axolotl.sh
@@ -3,4 +3,6 @@ set -e
 
 echo "Installing Axolotl..."
 cd vendor/axolotl/
+unzip -o axolotl-0.5.2.zip
+cd axolotl-0.5.2
 pip install -e .
--- scripts/install_deepspeed.sh
+++ scripts/install_deepspeed.sh
@@ -6,17 +6,19 @@ DEEPSPEED_COMMIT="bf03f4835295bccc4765c9ae428c5d3e3ddb04fe"
 echo "Installing DeepSpeed..."
 pip uninstall deepspeed -y
 
-# Définir le nombre max de jobs pour la compilation
+# Set maximum number of jobs for compilation
 export MAX_JOBS="$(nproc)"
+# Set architecture for PyTorch
+export PYTORCH_ROCM_ARCH="gfx90a;gfx942"
 
-# Cloner à un commit spécifique dans un dossier temporaire
+# Clone to a specific commit in a temporary folder
 git clone https://github.com/microsoft/DeepSpeed.git tmp_deepspeed
 cd tmp_deepspeed
 git checkout $DEEPSPEED_COMMIT
 
-# Installation avec les mêmes options que votre script original
+# Install with the same options as the original script
 pip install --verbose --no-cache-dir --no-build-isolation ./
 
-# Nettoyage
+# Cleanup
 cd ..
 rm -rf tmp_deepspeed
